864bd73cf6fe1c19537357295dff414889da3efc
Richard Hadden <richard.hadden@oeaw.ac.at>
2023-08-21 13:20:51 +0200

    can initialise models

--- pangloss_core/models.py
+++ pangloss_core/models.py
@@ -1,80 +1,128 @@
 from __future__ import annotations
 
-from annotated_types import MaxLen, MinLen, BaseMetadata
-from typing import Any, Optional, Annotated, Sequence, Generic, Union, TypeVar, ClassVar
-from typing_extensions import Unpack
-from pydantic import BaseModel as PydanticBaseModel, create_model as pydantic_create_model
+import inspect
+from dataclasses import dataclass
+from functools import lru_cache
+from typing import (
+    Annotated,
+    Any,
+    ClassVar,
+    Generic,
+    Literal,
+    Optional,
+    Sequence,
+    TypeVar,
+    Union,
+)
+from uuid import UUID
+
+import pydantic
+from annotated_types import BaseMetadata, MaxLen, MinLen
 from icecream import ic
 from pydantic.config import ConfigDict
-import inspect
-from functools import lru_cache
-from dataclasses import dataclass
-from uuid import UUID
+from typing_extensions import Unpack
+
 from pangloss_core.exceptions import PanglossConfigError
+
 
 class ModelManager:
     pass
 
+
 RELATION_IDENTIFIER = "__relation__"
 
-class BaseNodeReference(PydanticBaseModel):
+
+class RelationModel(pydantic.BaseModel):
+    pass
+
+
+class BaseNodeStandardFields(pydantic.BaseModel):
     __abstract__ = True
-    
+
     # Standard fields for all Reference types
     uid: UUID
     label: Annotated[str, MaxLen(500)]
 
-class BaseNode(BaseNodeReference):
+
+class BaseNodeReference(BaseNodeStandardFields):
+    relation_data: Optional[RelationModel | type[RelationModel]] = RelationModel()
+    real_type: str = ""
+
+    def __hash__(self):
+        return hash(self.uid)
+
+
+class BaseNode(BaseNodeStandardFields):
     """Base Node should be Abstract by default"""
+
     __abstract__ = True
-    
+
     Reference: ClassVar[type[BaseNodeReference]]
     RelationsTo: ClassVar[dict[str, type[BaseNode]]]
-    
+
     @classmethod
     def __pydantic_init_subclass__(cls, **kwargs: dict[str, Any]) -> None:
         """
         Things to do here:
-        
+
         1. Remove Trait classes: TICK
         2. Build labels: TICK
         3. Add class to ModelManager
         4. Build lists of Child Nodes, Relations, Reverse-Relations, RelatedReifications, ReverseRelatedReifications
-        
+
         """
-        
-        
-        #ic(cls, cls.__pros_traits_as_direct_ancestor__())
-        #ic(cls, cls.__pros_traits_as_indirect_ancestor__)
+
         cls.__pg_delete_indirect_trait_fields__()
         cls.model_rebuild(force=True)
-        cls.Reference = type(f"{cls.__name__}Reference", (BaseNodeReference,), {})
-    
+        cls.Reference = cls.__pg_create_reference_class__()
+
+    @classmethod
+    def __pg_create_reference_class__(
+        cls, relation_data_model: type[RelationModel] | None = None
+    ) -> type[BaseNodeReference]:
+        if not relation_data_model:
+            return pydantic.create_model(
+                f"{cls.__name__}Reference",
+                __base__=BaseNodeReference,
+                real_type=(Literal[cls.__name__.lower()], cls.__name__.lower()),  # type: ignore
+            )
+        else:
+            return pydantic.create_model(
+                f"{relation_data_model.__name__}_{cls.__name__}Reference",
+                __base__=BaseNodeReference,
+                real_type=(Literal[cls.__name__.lower()], cls.__name__.lower()),  # type: ignore
+                relation_data=(relation_data_model, ...),
+            )
+
     @classmethod
     @property
     @lru_cache
     def __pg_model_labels__(cls) -> list[str]:
         """All neo4j labels for model.
-        
+
         Includes direct Trait names."""
-        return [c.__name__ for c in cls.mro() if (issubclass(c, BaseNode) and c is not BaseNode) or c in cls.__pg_traits_as_direct_ancestors__]
-    
-   
-    
-    
+        return [
+            c.__name__
+            for c in cls.mro()
+            if (issubclass(c, BaseNode) and c is not BaseNode)
+            or c in cls.__pg_traits_as_direct_ancestors__
+        ]
+
     @classmethod
     def __pg_delete_indirect_trait_fields__(cls) -> None:
         trait_fields_to_delete = set()
         for trait in cls.__pg_traits_as_indirect_ancestors__:
             for field_name in cls.model_fields:
                 # AND AND... not in the parent class annotations that is *not* a trait...
-                if field_name in trait.__annotations__ and trait not in cls.__annotations__:
+                if (
+                    field_name in trait.__annotations__
+                    and trait not in cls.__annotations__
+                ):
                     trait_fields_to_delete.add(field_name)
-                    
+
         for field_to_delete in trait_fields_to_delete:
             del cls.model_fields[field_to_delete]
-    
-    
+
     @classmethod
     @property
     @lru_cache
@@ -90,8 +138,7 @@ class BaseNode(BaseNodeReference):
                 else:
                     continue
         return set(traits_as_direct_bases)
-    
-    
+
     @classmethod
     @property
     @lru_cache
@@ -99,10 +146,16 @@ class BaseNode(BaseNodeReference):
         traits_as_indirect_ancestors = []
         traits_as_direct_ancestors = cls.__pg_traits_as_direct_ancestors__
         for c in cls.mro():
-            if issubclass(c, AbstractTrait) and not issubclass(c, BaseNode) and c is not AbstractTrait and c is not cls and c not in traits_as_direct_ancestors:
+            if (
+                issubclass(c, AbstractTrait)
+                and not issubclass(c, BaseNode)
+                and c is not AbstractTrait
+                and c is not cls
+                and c not in traits_as_direct_ancestors
+            ):
                 traits_as_indirect_ancestors.append(c)
         return set(traits_as_indirect_ancestors)
-    
+
     @classmethod
     @lru_cache
     def __pg_all_subclasses__(cls) -> set[type[BaseNode]]:
@@ -111,60 +164,76 @@ class BaseNode(BaseNodeReference):
             subclasses += [subclass, *subclass.__pg_all_subclasses__()]
         return set(subclasses)
 
-class RelationModel(PydanticBaseModel):
-    pass
 
 @dataclass
 class RelationConfig:
     reverse_name: str
-    relation_to_base: Optional[type[BaseNode]] = None
-    relation_model: Optional[RelationModel] = None
+    relation_to_base: ClassVar[Optional[type[BaseNode]]] = None
+    relation_model: Optional[type[RelationModel]] = None
     validators: Optional[Sequence[BaseMetadata]] = None
-    
-    
+
+
 RelationToType = TypeVar("RelationToType", type[BaseNode], type[BaseNode])
 
-class RelationTo(PydanticBaseModel):
+
+class RelationTo(pydantic.BaseModel):
     """Defines a RelationTo type annotation, e.g.:
-    
+
     ```
     class Person(BaseNode):
         owns_pet: RelationTo[Pet, RelationConfig(reverse_name="is_owned_by")]
-    ```  
+    ```
     The `RelationConfig` is required, and must at least provide a `reverse_name`.
     """
-    
-    def __class_getitem__(cls, args: tuple[RelationToType, RelationConfig]) -> set[type[RelationToType]]:
-        """Creates a Pydantic-friendly Annotated type """
+
+    def __class_getitem__(
+        cls, args: tuple[RelationToType, RelationConfig]
+    ) -> set[type[RelationToType]]:
+        """Creates a Pydantic-friendly Annotated type"""
         try:
             related_type: type[BaseNode] = args[0]
         except TypeError:
-            raise PanglossConfigError("A RelationConfig instance must be provided as part of the type annotation")
-        
+            raise PanglossConfigError(
+                "A RelationConfig instance must be provided as part of the type annotation"
+            )
+
         relation_config: RelationConfig = args[1]
-        
+
         # Set the relation_config relation_to_base to the actual class concerned
-        relation_config.relation_to_base = related_type
-        
+        # Ignore type checking as we're cheating here...
+        relation_config.relation_to_base = related_type  # type: ignore
+
         # Get the subclasses of related-to cls, as possible allowed types
-        related_types = tuple([related_type.Reference, *[rt.Reference for rt in related_type.__pg_all_subclasses__()]])
-        
+        related_types = tuple(
+            [
+                related_type.__pg_create_reference_class__(
+                    relation_data_model=relation_config.relation_model
+                ),
+                *[
+                    rt.__pg_create_reference_class__(
+                        relation_data_model=relation_config.relation_model
+                    )
+                    for rt in related_type.__pg_all_subclasses__()
+                ],
+            ]
+        )
+
         # Check if config validators are set, and if not, make an empty list for unpacking below
         validators = relation_config.validators if relation_config.validators else []
-        
+
         # Return a Pydantic-friendly Annotated[] type
         # Need to do this # type: ignore hack here, as we are basically lying to the type checker
         # about what we are returning (dynamically constructing a list of sub-types)
-        return Annotated[set[Union[*related_types]], *validators, relation_config, RELATION_IDENTIFIER] # type: ignore
-
-
+        return Annotated[set[Union[*related_types]], *validators, relation_config, RELATION_IDENTIFIER]  # type: ignore
 
 
 class AbstractTrait:
     pass
 
-class AbstractMixin(PydanticBaseModel):
+
+class AbstractMixin(pydantic.BaseModel):
     pass
+
 
 """
 class Purchaseable(AbstractTrait):
@@ -202,5 +271,5 @@ npv = NonPurchaseableVegetable(uid="something", label="something", name="Swede",
 ic(npv.model_dump())
 """
 
-#ic(Person.model_fields)
-#ic(Person.model_json_schema())
+# ic(Person.model_fields)
+# ic(Person.model_json_schema())
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,119 +1,202 @@
 from uuid import uuid4
-from annotated_types import MaxLen
-import pytest
-from pangloss_core.models import BaseNode, AbstractTrait, RelationTo, RelationConfig, RELATION_IDENTIFIER
-from pangloss_core.exceptions import PanglossConfigError
 
-    
+import pytest
+from annotated_types import MaxLen
+
+from pangloss_core.exceptions import PanglossConfigError
+from pangloss_core.models import (
+    RELATION_IDENTIFIER,
+    AbstractTrait,
+    BaseNode,
+    RelationConfig,
+    RelationModel,
+    RelationTo,
+)
+
+
 def test_model_init():
     """Test initialisation of a BaseNode.
-    
-    Basic sanity check to make sure any other modifications of BaseNode don't prevent initialisation"""
+
+    Basic sanity check to make sure any other modifications of BaseNode don't prevent initialisation
+    """
+
     class Thing(BaseNode):
         name: str
         age: int
-        
+
     thing = Thing(uid=uuid4(), label="Someone", name="Someone", age=42)
-    
+
     assert thing.label == "Someone"
+
 
 def test_basic_field_inheritance():
     """Test fields models are inherited with appropriate fields"""
+
     class Animal(BaseNode):
         name: str
-    
+
     class Pet(Animal):
         pass
-    
+
     assert "name" in Animal.model_fields
     assert "name" in Pet.model_fields
 
+
 def test_model_labels():
     """Test labels are inherited"""
+
     class Animal(BaseNode):
         name: str
-    
+
     class Pet(Animal):
         pass
 
     assert Animal.__pg_model_labels__ == ["Animal"]
     assert set(Pet.__pg_model_labels__) == set(["Pet", "Animal"])
-    
-    
-    
+
+
 def test_traits_are_inherited_directly():
     """Test Traits are inherited by direct child"""
+
     class Purchaseable(AbstractTrait):
         price: int
-        
+
     class Animal(BaseNode):
         name: str
-    
+
     class Pet(Animal, Purchaseable):
         age: int
-    
+
     assert "name" in Pet.model_fields
     assert "age" in Pet.model_fields
     assert "price" in Pet.model_fields
-    
+
     assert set(Pet.__pg_model_labels__) == set(["Purchaseable", "Animal", "Pet"])
-    
+
+
 def test_traits_are_not_inherited_indirectly():
     """Test Traits are not inherited by grandchild classes"""
-    
+
     class Purchaseable(AbstractTrait):
         price: int
-        
+
     class Animal(BaseNode):
         name: str
-    
+
     class Pet(Animal, Purchaseable):
         age: int
-    
+
     class NonPurchaseablePet(Pet):
         pass
-    
+
     # Check we have the expected inherited fields
     assert "name" in NonPurchaseablePet.model_fields
     assert "age" in NonPurchaseablePet.model_fields
-    
+
     # But not the price, as we should not inherit from Purchaseable
     assert "price" not in NonPurchaseablePet.model_fields
-    
-    assert set(NonPurchaseablePet.__pg_model_labels__) == set(["NonPurchaseablePet", "Animal", "Pet"])
+
+    assert set(NonPurchaseablePet.__pg_model_labels__) == set(
+        ["NonPurchaseablePet", "Animal", "Pet"]
+    )
+
 
 def test_relation_to_fails_without_relationconfig():
     """Test using a RelationTo without a RelationConfig object throws an ConfigError"""
     with pytest.raises(PanglossConfigError):
+
         class Pet(BaseNode):
             pass
-        
+
         class Person(BaseNode):
             pets: RelationTo[Pet]
+
 
 def test_relation_to():
     class Pet(BaseNode):
         pass
-    
+
     class Crocodile(Pet):
         pass
-    
+
     class Person(BaseNode):
-        pets: RelationTo[Pet, RelationConfig(reverse_name="is_owned_by", validators=[MaxLen(2)])]
-        
+        pets: RelationTo[
+            Pet, RelationConfig(reverse_name="is_owned_by", validators=[MaxLen(2)])
+        ]
+
     assert "pets" in Person.model_fields
-    assert Person.model_fields["pets"].annotation.__args__[0].__args__[0].__name__ == "PetReference" # type: ignore
-    assert Person.model_fields["pets"].annotation.__args__[0].__args__[1].__name__ == "CrocodileReference" # type: ignore
-  
-    maxlen_validator, relation_config, relation_identifier = Person.model_fields["pets"].metadata
+    assert Person.model_fields["pets"].annotation.__args__[0].__args__[0].__name__ == "PetReference"  # type: ignore
+    assert Person.model_fields["pets"].annotation.__args__[0].__args__[1].__name__ == "CrocodileReference"  # type: ignore
+
+    maxlen_validator, relation_config, relation_identifier = Person.model_fields[
+        "pets"
+    ].metadata
     assert maxlen_validator == MaxLen(2)
     assert relation_config.reverse_name == "is_owned_by"
     assert relation_config.relation_to_base == Pet
     assert relation_identifier == RELATION_IDENTIFIER
-    
+
     schema = Person.model_json_schema()
- 
-    assert {'$ref': '#/$defs/PetReference'} in schema["properties"]["pets"]["items"]["anyOf"]
-    assert {'$ref': '#/$defs/CrocodileReference'} in schema["properties"]["pets"]["items"]["anyOf"]
-    
-    
+
+    assert {"$ref": "#/$defs/PetReference"} in schema["properties"]["pets"]["items"][
+        "anyOf"
+    ]
+    assert {"$ref": "#/$defs/CrocodileReference"} in schema["properties"]["pets"][
+        "items"
+    ]["anyOf"]
+
+
+"""
+def test_build_reference_model():
+    class Person(BaseNode):
+        pass
+
+    assert Person.Reference
+    p = Person.Reference(uid=uuid4(), label="Tom Jones")
+    assert p.real_type == "person"
+"""
+
+
+def test_reference_model_on_relation():
+    class PersonPetRelation(RelationModel):
+        cost_of_purchase: int
+
+    class Pet(BaseNode):
+        pass
+
+    class Crocodile(Pet):
+        pass
+
+    class Person(BaseNode):
+        pets: RelationTo[
+            Pet,
+            RelationConfig(
+                reverse_name="is_owned_by",
+                validators=[MaxLen(2)],
+                relation_model=PersonPetRelation,
+            ),
+        ]
+
+    print(Person.model_json_schema())
+    p = Person(
+        **{
+            "uid": uuid4(),
+            "label": "Tom Jones",
+            "pets": [
+                {
+                    "uid": uuid4(),
+                    "label": "Mr Frisky",
+                    "real_type": "pet",
+                    "relation_data": {"cost_of_purchase": 200},
+                },
+                {
+                    "uid": uuid4(),
+                    "label": "Mr Snappy",
+                    "real_type": "crocodile",
+                    "relation_data": {"cost_of_purchase": 200},
+                },
+            ],
+        }
+    )
+    print(Person.model_json_schema())
+    assert len(p.pets) == 2
